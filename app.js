d17b3ed8b28b66b38598df598f56d8cd75e20267
Siddharth Gulati <sid441.sg@gmail.com>
2022-02-18 19:36:15 -0500

    Project directory

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const configRoutes = require("./routes");
+const data = require("./data");
+
+app.use(express.json());
+
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});
