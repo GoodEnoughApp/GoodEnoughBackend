62ae970aff4aab6a35e1113a76fb1b0ff70b8ba9
Kavish Sanghvi <sanghvi_kavish@yahoo.in>
2022-03-01 13:15:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // model.users.findAll().then((data) => console.log(data));
 configRoutes(app);
 app.use(express.urlencoded({ extended: true }));
-app.listen(3000, () => {
+app.listen(52560, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:3000');
 });
