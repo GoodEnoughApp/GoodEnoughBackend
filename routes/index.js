4c6d3be5bd289109be4e37c8f1e19c0dfc056b40
Jose J. Cruz <jjzcru@gmail.com>
2022-04-05 00:11:03 -0400

    feat: :sparkles: Add health check

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ const products = require('./products');
 const category = require('./category');
 const items = require('./items');
 const shopping = require('./shopping');
+
 const constructorMethod = (app) => {
   app.use('/', users);
   app.use('/products', products);
   app.use('/categories', category);
   app.use('/items', items);
   app.use('/shopping', shopping);
+  app.get('/health', (_, res) => {
+    res.send({ success: true });
+  });
   app.use('*', (req, res) => {
     res.sendStatus(404);
   });
